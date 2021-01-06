0c25d4f65750e796232c2bdd2e75cc6eae5d3799
Alexander <jalxby@gmail.com>
2021-01-06 20:23:08 +0300

    third

--- home_work_1/Sleepy.java
+++ home_work_1/Sleepy.java
@@ -2,21 +2,21 @@ package home_work_1;
 
 public class Sleepy {
 
-    public static boolean sleepIn(boolean weekday, boolean vacation){
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
 
-        if(!weekday || vacation){
+        if (!weekday || vacation) {
             return true;
         }
-
-    return false;
+        return false;
     }
+
     public static void main(String[] args) {
 
         boolean sleep = sleepIn(false, false);
 
-        if (sleep){
+        if (sleep) {
             System.out.println("Можем спать дальше");
-        }else if(!sleep) {
+        } else if (!sleep) {
             System.out.println("Пора идти на работу");
         }
     }
