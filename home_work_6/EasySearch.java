82dc9919407615786bf0619f27ecc6999653d3e3
Alexander Jaroshevich <jalxby@gmail.com>
2021-02-28 23:02:22 +0300

    commit10

---
+++ home_work_6/EasySearch.java
@@ -0,0 +1,10 @@
+package home_work_6;
+
+import home_work_6.api.ISearchEngine;
+
+public class EasySearch implements ISearchEngine {
+    @Override
+    public long search(String text, String word) {
+        return 0;
+    }
+}
